9d6369c109c2f53509a14cb17769f3842d6ae2ba
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2026-03-11 21:35:25 +0300

    add warehouse

---
+++ src/DddSample.Domain/Warehouse.cs
@@ -0,0 +1,18 @@
+﻿namespace DddSample.Domain;
+
+public sealed class Warehouse : AggregateRoot
+{
+  public Warehouse(WarehouseId id, WarehouseAddress address, WarehouseContact contact)
+  {
+    Id = id;
+    Address = address ?? throw new ArgumentNullException(nameof(address));
+    Contact = contact ?? throw new ArgumentNullException(nameof(contact));
+  }
+
+  public WarehouseId Id { get; }
+
+  public WarehouseAddress Address { get; }
+
+  public WarehouseContact Contact { get; }
+}
+
