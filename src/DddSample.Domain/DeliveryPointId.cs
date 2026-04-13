b2c72d9a7795a5e5a7991ecc46313926c5faee6d
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2026-04-13 17:53:33 +0300

    make id equatable

--- src/DddSample.Domain/DeliveryPointId.cs
+++ src/DddSample.Domain/DeliveryPointId.cs
@@ -1,12 +1,25 @@
-﻿namespace DddSample.Domain;
+﻿using System.Diagnostics.CodeAnalysis;
 
-public readonly struct DeliveryPointId
+namespace DddSample.Domain;
+
+public readonly struct DeliveryPointId : IEquatable<DeliveryPointId>
 {
   public static readonly DeliveryPointId None;
 
   private readonly Guid _id;
 
   private DeliveryPointId(Guid id) => _id = id;
+
+  public override int GetHashCode() => _id.GetHashCode();
+
+  public bool Equals(DeliveryPointId other) => _id == other._id;
+
+  public override bool Equals([NotNullWhen(true)] object? obj)
+  {
+    if (obj is null) return false;
+    if (obj is not DeliveryPointId other) return false;
+    return Equals(other);
+  }
 
   public override string ToString() => _id.ToString();
 
@@ -15,5 +28,8 @@ public readonly struct DeliveryPointId
 
   public static implicit operator DeliveryPointId(Guid value) => new(value);
   public static explicit operator Guid(DeliveryPointId value) => value._id;
+
+  public static bool operator ==(DeliveryPointId left, DeliveryPointId right) => Equals(left, right);
+  public static bool operator !=(DeliveryPointId left, DeliveryPointId right) => !Equals(left, right);
 }
 
