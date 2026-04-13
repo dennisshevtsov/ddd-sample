64b2ccac00510c5b97a3636e841157b1a523a1e1
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2026-04-13 17:35:35 +0300

    refactor converters

---
+++ src/DddSample.Infrastructure/JsonSerializerOptionsExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace DddSample.Infrastructure;
+
+internal static class JsonSerializerOptionsExtensions
+{
+  internal static string ConvertName(this JsonSerializerOptions options, string name)
+  {
+    if (options.PropertyNamingPolicy is null)
+    {
+      return name;
+    }
+    return options.PropertyNamingPolicy.ConvertName(name);
+  }
+}
